465e34b7a9c92357651889c0a31b20ced8f77753
ronco <ronco@costite.com>
2015-05-25 09:13:07 -0600

    Ensure context is set for meta function
    fixes #5

--- tests/dummy/app/routes/route-1.js
+++ tests/dummy/app/routes/route-1.js
@@ -2,10 +2,11 @@ import Ember from 'ember';
 import RouteMetaMixin from 'ember-cli-meta-tags/mixins/route-meta';
 
 export default Ember.Route.extend(RouteMetaMixin, {
+  name: 'Ice-T',
   meta: function() {
     return {
       'property': {
-        'og:name': 'Ice-T'
+        'og:name': this.get('name')
       }
     };
   }
