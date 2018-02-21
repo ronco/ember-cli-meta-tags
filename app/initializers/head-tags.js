eb6bfbabdbc725e0fbfcd853b48162dfd5cdcb11
snewcomer <snewcomer24@gmail.com>
2018-02-20 21:11:29 -0800

    Router.reopen with didTransition

--- app/initializers/head-tags.js
+++ app/initializers/head-tags.js
@@ -1,10 +1,1 @@
-export function initialize() {
-  // ember 1.13 backwards compatibility
-  const application = arguments[1] || arguments[0];
-  application.inject('service:head-tags', 'router', 'router:main');
-}
-
-export default {
-  name: 'head-tags',
-  initialize: initialize
-};
+export { default, initialize } from 'ember-cli-meta-tags/initializers/head-tags';
