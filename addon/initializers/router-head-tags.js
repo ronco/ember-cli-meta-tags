eec6ed368ea05a2933a685fdd46cf725e549ae7c
snewcomer <snewcomer24@gmail.com>
2019-01-17 11:45:28 -0800

    initializer compat pre 3.6

--- addon/initializers/router-head-tags.js
+++ addon/initializers/router-head-tags.js
@@ -1,20 +1,32 @@
 import Router from '@ember/routing/router';
 import { get } from '@ember/object';
 import { inject as service } from '@ember/service';
+import { gte } from 'ember-compatibility-helpers';
 
 export function initialize() {
-  Router.reopen({
-    headTags: service(),
+  if (gte('3.6.0-beta.1')) {
+    Router.reopen({
+      headTags: service(),
 
-    init() {
-      this._super(...arguments);
+      init() {
+        this._super(...arguments);
 
-      this.on('routeDidChange', () => {
+        this.on('routeDidChange', () => {
+          get(this, 'headTags').collectHeadTags();
+          this._super(...arguments);
+        });
+      }
+    });
+  } else {
+    Router.reopen({
+      headTags: service(),
+
+      didTransition() {
         get(this, 'headTags').collectHeadTags();
         this._super(...arguments);
-      });
-    }
-  });
+      }
+    });
+  }
 }
 
 export default {
