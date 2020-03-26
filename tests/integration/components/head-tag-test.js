aacebe170ca8d2ebdaa7063e769ce9f5f91a297b
snewcomer <snewcomer24@gmail.com>
2020-03-25 21:00:38 -0700

    remove precompile

--- tests/integration/components/head-tag-test.js
+++ tests/integration/components/head-tag-test.js
@@ -2,8 +2,7 @@ import { A } from '@ember/array';
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
-
+import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | head tag', function(hooks) {
   setupRenderingTest(hooks);
