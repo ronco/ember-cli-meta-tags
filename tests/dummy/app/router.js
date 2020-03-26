0480d7fcc802a8c1ab7c3f8581bab5df752044e3
snewcomer <snewcomer24@gmail.com>
2020-03-25 21:04:45 -0700

    add back router

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,10 +1,21 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
-}
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
 
 Router.map(function() {
+  this.route('route-1');
+  this.route('route-2');
+  this.route('route-object-1');
+  this.route('route-object-2');
+  this.route('resource', function() {
+    this.route('sub', { resetNamespace: true }, function() {
+      this.route('deep');
+    });
+  });
 });
+
+export default Router;
