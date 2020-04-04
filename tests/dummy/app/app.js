c74aba7ba0652f600ed2587537e05c9a344c1b38
snewcomer <snewcomer24@gmail.com>
2020-04-04 15:36:22 -0700

    revet app.js to master

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,12 +1,14 @@
 import Application from '@ember/application';
-import Resolver from 'ember-resolver';
+import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
-export default class App extends Application {
-  modulePrefix = config.modulePrefix;
-  podModulePrefix = config.podModulePrefix;
-  Resolver = Resolver;
-}
+const App = Application.extend({
+  modulePrefix: config.modulePrefix,
+  podModulePrefix: config.podModulePrefix,
+  Resolver
+});
 
 loadInitializers(App, config.modulePrefix);
+
+export default App;
